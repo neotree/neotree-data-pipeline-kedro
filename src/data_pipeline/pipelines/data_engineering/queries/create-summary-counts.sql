ed7b3711034eb15344e7829066e2900497376499
mbaradza <mbaradza@hitrac.co.zw>
2021-03-10 11:25:26 +0200

    Renamed the columns of summary-counts-tables

--- src/data_pipeline/pipelines/data_engineering/queries/create-summary-counts.sql
+++ src/data_pipeline/pipelines/data_engineering/queries/create-summary-counts.sql
@@ -3,19 +3,19 @@ CREATE TABLE derived.summary_counts AS
 SELECT  
 derived.summary_joined_admissions_discharges."AdmissionMonthYear" AS "AdmissionMonthYear",
         derived.summary_joined_admissions_discharges."AdmissionMonthYearSort" AS "AdmissionMonthYearSort", 
-        sum(derived.summary_joined_admissions_discharges."AdmissionCount") AS "sum", 
-        sum(derived.summary_joined_admissions_discharges."DischargeCount") AS "sum_2", 
-        sum(derived.summary_joined_admissions_discharges."DeathCount") AS "sum_3",
-        sum(derived.summary_joined_admissions_discharges."Death<24hrsCount") AS "sum_4",
-        sum(derived.summary_joined_admissions_discharges."Death>24hrsCount") AS "sum_5", 
-        sum(derived.summary_joined_admissions_discharges."TransferredOutCount") AS "sum_6", 
-        sum(derived.summary_joined_admissions_discharges."AbscondedCount") AS "sum_7", 
-        sum(derived.summary_joined_admissions_discharges."DischargeOnRequestCount") AS "sum_8", 
-        sum(derived.summary_joined_admissions_discharges."WithinFacilityCount") AS "sum_9", 
-        sum(derived.summary_joined_admissions_discharges."OutsideFacilityCount") AS "sum_10",
-        sum(derived.summary_joined_admissions_discharges."PrematureCount") AS "sum_11", 
-        sum(derived.summary_joined_admissions_discharges."HypothermiaCount") AS "sum_12", 
-        sum(derived.summary_joined_admissions_discharges."Less28wks/1kgCount") AS "sum_13"
+        sum(derived.summary_joined_admissions_discharges."AdmissionCount") AS "TotalAdmissions", 
+        sum(derived.summary_joined_admissions_discharges."DischargeCount") AS "TotalDischarges", 
+        sum(derived.summary_joined_admissions_discharges."DeathCount") AS "TotalDeaths",
+        sum(derived.summary_joined_admissions_discharges."Death<24hrsCount") AS "TotalDeaths<24hrs",
+        sum(derived.summary_joined_admissions_discharges."Death>24hrsCount") AS "TotalDeaths>24hrs", 
+        sum(derived.summary_joined_admissions_discharges."TransferredOutCount") AS "TotalTransferredOut", 
+        sum(derived.summary_joined_admissions_discharges."AbscondedCount") AS "TotalAbsconded", 
+        sum(derived.summary_joined_admissions_discharges."DischargeOnRequestCount") AS "TotalDischargeOnRequest", 
+        sum(derived.summary_joined_admissions_discharges."WithinFacilityCount") AS "TotalAdmittedFronWithinFacility", 
+        sum(derived.summary_joined_admissions_discharges."OutsideFacilityCount") AS "TotalAdmittedFromOutsideFacility",
+        sum(derived.summary_joined_admissions_discharges."PrematureCount") AS "TotalPremBabies", 
+        sum(derived.summary_joined_admissions_discharges."HypothermiaCount") AS "TotalHypothemiaBabies", 
+        sum(derived.summary_joined_admissions_discharges."Less28wks/1kgCount") AS "TotalBabiesWith<28wksAnd1kgs"
 FROM derived.summary_joined_admissions_discharges
 GROUP BY derived.summary_joined_admissions_discharges."AdmissionMonthYear", 
 derived.summary_joined_admissions_discharges."AdmissionMonthYearSort"
