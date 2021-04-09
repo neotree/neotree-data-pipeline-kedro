551a34c48f1d5279fc2c8b0db41dc2cec05a350f
mbaradza <mbaradza@hitrac.co.zw>
2021-04-09 12:24:03 +0200

    ...

--- src/data_pipeline/pipelines/data_engineering/data_tyding/explode_mcl_columns.py
+++ src/data_pipeline/pipelines/data_engineering/data_tyding/explode_mcl_columns.py
@@ -1,4 +1,5 @@
 from conf.common.sql_functions import create_table,append_data
+from data_pipeline.pipelines.data_engineering.queries.check_table_exists_sql import table_exists
 # this function explodes all mcl columns and creates respective tables
 # This input is a set of columns that need to be exploded
 # no output but tables in postgres are created, tables names are the same as mcl column titles
@@ -16,10 +17,12 @@ def explode_column(df, mcl):
             mcl_column_exp = mcl_column.explode(column)
             mcl_column_exp['uid'] = df['uid']
             #Drop All Rows With Label Other, The Values Will Be Collected In The Method Explode_Other_Columns
-            mcl_column_exp = mcl_column_exp.loc[(mcl_column_exp[column] != "Other") & (mcl_column_exp[column].notna()) ]
+            mcl_column_exp = mcl_column_exp.loc[(mcl_column_exp[column] != "Other") & (mcl_column_exp[column].notna())]
             mcl_column_exp.set_index('uid')
             mcl_column_exp.reindex(columns =mcl_column_exp.columns)
             column_name = ("exploded_"+column)
+            schema = 'derived'
+            table_exists(schema,column_name)
             create_table(mcl_column_exp, column_name)
 
 def explode_other_column(df,mcl):
@@ -38,6 +41,8 @@ def explode_other_column(df,mcl):
             mcl_column_exp.drop(column,axis='columns',inplace=True)
             mcl_column_exp.reindex(columns =mcl_column_exp.columns)
             column_name = ("exploded_"+parent_column)
+            schema = 'derived'
+            table_exists(schema,column_name)
             append_data(mcl_column_exp, column_name)
         else:
             pass;
---
+++ src/data_pipeline/pipelines/data_engineering/queries/check_table_exists_sql.py
@@ -0,0 +1,10 @@
+from conf.common.sql_functions import inject_sql_with_return
+
+def table_exists(schema, table_name):
+    query = ''' SELECT EXISTS (
+                SELECT FROM information_schema.tables 
+                WHERE  table_schema = '{0}'
+                AND    table_name   = '{1}'
+                );'''.format(schema,table_name)
+    query_result = inject_sql_with_return(query);
+    print("@@@@@@@@@@@@@@---",query_result)
