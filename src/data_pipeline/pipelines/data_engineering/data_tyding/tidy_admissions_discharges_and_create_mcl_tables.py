210972e5b9f1a649ebb003ff8f00ffc88a35eea2
mbaradza <mbaradza@gmail.com>
2022-01-10 20:06:36 +0200

    ..

--- src/data_pipeline/pipelines/data_engineering/data_tyding/tidy_admissions_discharges_and_create_mcl_tables.py
+++ src/data_pipeline/pipelines/data_engineering/data_tyding/tidy_admissions_discharges_and_create_mcl_tables.py
@@ -448,10 +448,10 @@ def tidy_tables():
                             if not bct_type_df.empty:
                                 preliminary_index= 1;
                                 for bct_index, row in bct_type_df.iterrows():
-                                    bct_value = neolab_df[(neolab_df['uid']
+                                    bct_value = neolab_df.loc[(neolab_df['uid']
                                             ==bct_type_df.at[bct_index,'uid']) & (neolab_df['DateBCT.value']
                                             ==bct_type_df.at[bct_index,'DateBCT.value']) & (neolab_df['DateBCR.value']
-                                            == bct_type_df.at[bct_index,'DateBCR.value'])]['BCType']
+                                            == bct_type_df.at[bct_index,'DateBCR.value']),'BCType'].iloc[0]
 
                                     logging.info("====BCT_V=",bct_value is None)
                                     if row['uid'] =='9980-0818':
