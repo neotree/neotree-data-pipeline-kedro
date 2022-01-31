0460d7cae3e86ad9c2e1e4bcc9baa0f2ba53fdca
mbaradza <mbaradza@gmail.com>
2022-01-31 21:26:39 +0200

    ..

--- src/data_pipeline/pipelines/data_engineering/utils/key_change.py
+++ src/data_pipeline/pipelines/data_engineering/utils/key_change.py
@@ -5,7 +5,7 @@ import sys
 def key_change(df: pd.DataFrame,row,position,old_key,new_key):
     try:
         if old_key in row and (row[old_key]) != 'nan' and new_key not in row:
-            df.loc[position,new_key] = row[old_key]
+            df.iloc[position,new_key] = row[old_key]
     except Exception as ex:
         logging.info("---CANT CONVERT--"+ex.with_traceback())
         sys.exit();
