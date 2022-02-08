dc5a14c2f37a2bb3b8009fdf6285d9a77c261476
mbaradza <mbaradza@gmail.com>
2022-02-08 07:46:14 +0200

    Format Date

--- src/data_pipeline/pipelines/data_engineering/utils/custom_date_formatter.py
+++ src/data_pipeline/pipelines/data_engineering/utils/custom_date_formatter.py
@@ -10,8 +10,8 @@ def format_date(df,field_name):
     """
     try: 
         if field_name in df and df[field_name] is not None and pd.notnull(df[field_name]):
-            #df[field_name] = dt.strftime(df[field_name].map(lambda x: str(x)), format='%Y-%m-%dT%H:%M:%S.%f')
-            df[field_name] = pd.to_datetime(df[field_name].map(lambda x: str(x)[:-4]), format='%Y-%m-%dT%H:%M:%S')
+            df[field_name] = dt.strftime(df[field_name].map(lambda x: str(x)), format='%Y-%m-%dT%H:%M:%S.%f')
+            #df[field_name] = pd.to_datetime(df[field_name].map(lambda x: str(x)[:-4]), format='%Y-%m-%dT%H:%M:%S')
 
         else:
             df[field_name]= None;
