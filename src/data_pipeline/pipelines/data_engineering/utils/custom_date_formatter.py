39a92bce1d2b945340450fd903162f7469c7b78e
mbaradza <mbaradza@gmail.com>
2022-02-08 18:02:17 +0200

    set date directly in df

--- src/data_pipeline/pipelines/data_engineering/utils/custom_date_formatter.py
+++ src/data_pipeline/pipelines/data_engineering/utils/custom_date_formatter.py
@@ -10,7 +10,7 @@ def format_date(df:pd.DataFrame,field_name):
     """
     try: 
         if field_name in df and df[field_name] is not None:
-            df[field_name] = pd.to_datetime(df[field_name].map(lambda x: str(x)[:-4]), errors='coerce',format='%Y-%m-%dT%H:%M:%S')
+            return pd.to_datetime(df[field_name].map(lambda x: str(x)[:-4]), errors='coerce',format='%Y-%m-%dT%H:%M:%S')
     except Exception as e:
         raise (e.with_traceback())
        
