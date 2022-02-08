7091335da53a053c4dbfa0ef8bd87584893c5e4d
mbaradza <mbaradza@gmail.com>
2022-02-08 12:39:07 +0200

    ...

--- src/data_pipeline/pipelines/data_engineering/utils/custom_date_formatter.py
+++ src/data_pipeline/pipelines/data_engineering/utils/custom_date_formatter.py
@@ -11,7 +11,7 @@ def format_date(df,field_name):
     try: 
         if field_name in df and df[field_name] is not None and pd.notnull(df[field_name]):
             #df[field_name] = dt.strftime(df[field_name].map(lambda x: str(x)), format='%Y-%m-%dT%H:%M:%S.%f')
-            df[field_name] = pd.to_datetime(df[field_name].map(lambda x: str(x)[:-4]), format='%Y-%m-%dT%H:%M:%S')
+            df[field_name] = pd.to_datetime(df[field_name].map(lambda x: str(x)[:-4]), format='%Y-%m-%dT%H:%M:%S').astype('datetime64[ns]')
 
         else:
             df[field_name]= None;
@@ -28,8 +28,12 @@ def format_date_without_timezone(df,field_name):
     try: 
         if field_name in df and df[field_name] is not None:
             df[field_name] = df[field_name].map(lambda x: str(x)[:-4])
-            df[field_name] = pd.to_datetime(df[field_name], format='%Y-%m-%dT%H:%M:%S')
+            df[field_name] = pd.to_datetime(df[field_name], format='%Y-%m-%dT%H:%M:%S').astype('datetime64[ns]')
         else:
             df[field_name]= None;
     except Exception as e:
         pass
+
+def convert_column_to_date(df: pd.DataFrame,column):
+    if column in df:
+        df[column] = df[column]
