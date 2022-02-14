a2cb79f1b16dd375b0258b20c39cbe4fa3a1ed98
mbaradza <mbaradza@gmail.com>
2022-02-14 16:48:15 +0200

    Cast Dates To Text before calling TO_DATE function

--- src/data_pipeline/pipelines/data_engineering/queries/assorted_queries.py
+++ src/data_pipeline/pipelines/data_engineering/queries/assorted_queries.py
@@ -315,14 +315,14 @@ def read_new_smch_admissions_query():
     return f'''
             select 
                 *
-            from derived.admissions where TO_DATE("DateTimeAdmission.value",'YYYY-MM-DD') >='2021-02-01' AND facility = 'SMCH';'''
+            from derived.admissions where TO_DATE(cast("DateTimeAdmission.value" as TEXT),'YYYY-MM-DD') >='2021-02-01' AND facility = 'SMCH';'''
 
 def read_new_smch_discharges_query():
     return f'''
             select 
                 *
-            from derived.discharges where TO_DATE("DateTimeDischarge.value",'YYYY-MM-DD') >='2021-02-01' 
-                or TO_DATE("DateTimeDeath.value",'YYYY-MM-DD')>'2021-02-01'  AND facility = 'SMCH';'''
+            from derived.discharges where TO_DATE(cast("DateTimeDischarge.value" as TEXT),'YYYY-MM-DD') >='2021-02-01' 
+                or TO_DATE(cast("DateTimeDeath.value",as TEXT),'YYYY-MM-DD')>'2021-02-01'  AND facility = 'SMCH';'''
 
 def read_old_smch_admissions_query():
     return f'''
