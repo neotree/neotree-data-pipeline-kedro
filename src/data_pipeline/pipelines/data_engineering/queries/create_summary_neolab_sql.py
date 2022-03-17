c8e682a9713a7f1982c6e76fbbf613495625bfe6
mbaradza <mbaradza@gmail.com>
2022-03-18 00:32:22 +0200

    Fix Query

--- src/data_pipeline/pipelines/data_engineering/queries/create_summary_neolab_sql.py
+++ src/data_pipeline/pipelines/data_engineering/queries/create_summary_neolab_sql.py
@@ -1,28 +1,33 @@
 
 #Query to create summary neolab table
 def summary_neolab_query():
-  return   f''' DROP TABLE IF EXISTS derived.summary_neolab;
-                CREATE TABLE derived.summary_neolab AS 
-                (
-                  WITH latest_neolab as (
-                  SELECT
-                    "facility",
-                    "uid",
-                    max("DateBCR.value") as "DateBCR.value" -- Take The Last Record
-                    from derived.neolab where uid not like '0000%' and uid not like '***%'
-                    group by 1,2
-                    )
-                    SELECT 
-                    latest_neolab."facility",
-                    latest_neolab."uid",
-                    latest_neolab."DateBCR.value" AS "DateBCR",
-                    derived.neolab."BCResult.value" AS "BCResult",
+  return   f''' drop table if exists derived.summary_neolab cascade;
+            create table derived.summary_neolab as 
+            (
+            with earliest_neolab as (
+            select
+            facility,
+            uid, 
+            min("DateBCR.value") as "DateBCR" -- This takes the first upload 
+                    -- of the session as the deduplicated record. 
+                    -- We could replace with max(id) to take the 
+                    -- most recently uploaded
+            from derived.neolab
+            where uid not like '0000%' and uid not like '***%'
+            group by 1,2
+            )
+            select
+            earliest_neolab.facility,
+            earliest_neolab.uid,
+            earliest_neolab."DateBCR",
+            derived.neolab."BCResult.value" AS "BCResult",
                     CASE WHEN 
                     derived.neolab."BCType" like '%PRELIMINARY%' THEN 'PRELIMINARY'
                     WHEN derived.neolab."BCType" like '%FINAL%' THEN 'FINAL'
                     END AS "Status",
                     derived.neolab."DateBCT.value" AS "DATEBCT",
-                    count(latest_neolab."uid") as "Total Blood Cultures"
-                    from latest_neolab join derived.neolab on
-                    latest_neolab."DateBCR.value" = derived.neolab."DateBCR.value"
-              );'''
+				    (select count(derived.neolab.uid) from derived.neolab where 
+				    earliest_neolab.uid=derived.neolab."uid") AS "Number Of Cultures"        
+            from earliest_neolab join derived.neolab
+            on earliest_neolab."DateBCR" = derived.neolab."DateBCR.value" 
+            );'''
