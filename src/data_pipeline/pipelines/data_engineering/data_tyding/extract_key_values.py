8e5f62a148f50826e21cfe23ca2b6880d64f8f1c
mbaradza <mbaradza@gmail.com>
2022-05-23 21:21:41 +0200

    Empty array check before processing labels and values

--- src/data_pipeline/pipelines/data_engineering/data_tyding/extract_key_values.py
+++ src/data_pipeline/pipelines/data_engineering/data_tyding/extract_key_values.py
@@ -1,5 +1,6 @@
 # This module extracts the key-value pairs within a raw json file.
 import logging
+from conf.common.format_error import formatError
 from .json_restructure import restructure, restructure_new_format, restructure_array
 from functools import reduce
 
@@ -9,58 +10,60 @@ def get_key_values(data_raw):
     data_new = []
     for index, row in data_raw.iterrows():
         # to store all the restructured keys & values for each row
-        new_entry = {}
-        # add uid and ingested_at first
-        app_version = None
-        if 'appVersion' in row:
-            app_version = row['appVersion']
-        if(app_version!=None and app_version!=''):
-            #Remove any Other Characters that are non-numeric
-            app_version = int(''.join(d for d in app_version if d.isdigit()))
-        if 'facility' in row:
-            new_entry['facility'] = row['facility']
-        
-        # Convert All UIDS TO UPPER CASE
-        new_entry['uid'] = str(row['uid']).upper()
-        if 'ingested_at_admission' in row:
-            new_entry['ingested_at'] = row['ingested_at_admission']
-        if 'ingested_at_discharge' in row:
-            new_entry['ingested_at'] = row['ingested_at_discharge']
+        try:
+            new_entry = {}
+            # add uid and ingested_at first
+            app_version = None
+            if 'appVersion' in row:
+                app_version = row['appVersion']
+            if(app_version!=None and app_version!=''):
+                #Remove any Other Characters that are non-numeric
+                app_version = int(''.join(d for d in app_version if d.isdigit()))
+            if 'facility' in row:
+                new_entry['facility'] = row['facility']
+            
+            # Convert All UIDS TO UPPER CASE
+            new_entry['uid'] = str(row['uid']).upper()
+            if 'ingested_at_admission' in row:
+                new_entry['ingested_at'] = row['ingested_at_admission']
+            if 'ingested_at_discharge' in row:
+                new_entry['ingested_at'] = row['ingested_at_discharge']
 
-        if 'started_at' in row:
-            new_entry['started_at'] = row['started_at']
+            if 'started_at' in row:
+                new_entry['started_at'] = row['started_at']
 
-        if 'started_at' in row:
-            new_entry['started_at'] = row['started_at']
+            if 'started_at' in row:
+                new_entry['started_at'] = row['started_at']
 
-        if 'completed_at' in row:
-             new_entry['completed_at'] = row['completed_at']
-        
-        if 'ingested_at' in row:
+            if 'completed_at' in row:
+                new_entry['completed_at'] = row['completed_at']
+            
+            if 'ingested_at' in row:
              new_entry['ingested_at'] = row['ingested_at']
 
 
         # iterate through key, value and add to dict
-        for c in row['entries']:
+            for c in row['entries']:
            
-            #RECORDS FORMATTED WITH NEW FORMAT, CONTAINS THE jsonFormat Key and C is the Key
-            if(app_version!='' and app_version!=None and (app_version>454 or int(str(app_version)[:1])>=5)): 
-                
-                k, v, mcl = restructure_new_format(c,row['entries'][c], mcl)
-                #SET UID FOR ZIM DISCHARGES WHICH COME WITH NULL UID NEW FORMAT
-                if((k=='NeoTreeID' or k=='NUID_BC' or k=='NUID_M' or k=='NUID_S') and new_entry['uid'] is None):
-                     new_entry['uid'] = v.value;
+                #RECORDS FORMATTED WITH NEW FORMAT, CONTAINS THE jsonFormat Key and C is the Key
+                if(app_version!='' and app_version!=None and (app_version>454 or int(str(app_version)[:1])>=5)):             
+                    k, v, mcl = restructure_new_format(c,row['entries'][c], mcl)
+                    #SET UID FOR ZIM DISCHARGES WHICH COME WITH NULL UID NEW FORMAT
+                    if((k=='NeoTreeID' or k=='NUID_BC' or k=='NUID_M' or k=='NUID_S') and new_entry['uid'] is None):
+                        new_entry['uid'] = v.value;
 
             #ELSE USE THE OLD FORMAT
-            else:
-               k, v, mcl = restructure(c, mcl)
-               #SET UID FOR ZIM DISCHARGES WHICH COME WITH NULL UID OLD FORMAT
-               if((k=='NeoTreeID' or k=='NUID_BC'or k=='NUID_M' or k=='NUID_S') and new_entry['uid'] is None):
-                     new_entry['uid'] = v.value;
-            new_entry[k] = v
+                else:
+                    k, v, mcl = restructure(c, mcl)
+                #SET UID FOR ZIM DISCHARGES WHICH COME WITH NULL UID OLD FORMAT
+                if((k=='NeoTreeID' or k=='NUID_BC'or k=='NUID_M' or k=='NUID_S') and new_entry['uid'] is None):
+                        new_entry['uid'] = v.value;
+                new_entry[k] = v
         # for each row add all the keys & values to a list
          
-        data_new.append(new_entry)
+            data_new.append(new_entry)
+        except Exception as ex:
+            logging.error(formatError(ex))
         
     return data_new, set(mcl)
 
