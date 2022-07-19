3b320e89f560326a08de61acc5e93dfbedc6eef0
mbaradza <mbaradza@gmail.com>
2022-07-19 20:05:38 +0200

    add query alias

--- src/data_pipeline/pipelines/data_engineering/queries/create_combined_diagnoses_sql.py
+++ src/data_pipeline/pipelines/data_engineering/queries/create_combined_diagnoses_sql.py
@@ -2,9 +2,9 @@ def combined_diagnoses_query():
  return f'''
              DROP TABLE IF EXISTS derived.combined_diagnoses;
              CREATE TABLE derived.combined_diagnoses AS
-             SELECT "uid" AS "uid", "Diagnoses.label" AS "Diagnoses", "Admissions__ingested_at" AS "ingested_at"
+             SELECT "source"."uid" AS "uid", "source"."Diagnoses.label" AS "Diagnoses", "source"."Admissions__ingested_at" AS "ingested_at"
              FROM (SELECT "derived"."exploded_Diagnoses.label"."uid" AS "uid", "derived"."exploded_Diagnoses.label"."Diagnoses.label" AS "Diagnoses.label", "derived"."exploded_Diagnoses.label"."facility" AS "facility", "Admissions"."ingested_at" AS "Admissions__ingested_at" FROM "derived"."exploded_Diagnoses.label"
-             LEFT JOIN "derived"."admissions" "Admissions" ON "derived"."exploded_Diagnoses.label"."uid" = "Admissions"."uid")
+             LEFT JOIN "derived"."admissions" "Admissions" ON "derived"."exploded_Diagnoses.label"."uid" = "Admissions"."uid") "source"
              UNION ALL
              SELECT "derived"."diagnoses"."uid" AS "uid", "derived"."diagnoses"."diagnosis" AS "diagnosis", "derived"."diagnoses"."ingested_at" AS "ingested_at"
              FROM "derived"."diagnoses";
