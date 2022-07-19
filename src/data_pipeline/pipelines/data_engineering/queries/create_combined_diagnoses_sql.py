846910a713cd3444a0b5b1a19a9c1e3b187f2214
mbaradza <mbaradza@gmail.com>
2022-07-19 19:50:35 +0200

    .

--- src/data_pipeline/pipelines/data_engineering/queries/create_combined_diagnoses_sql.py
+++ src/data_pipeline/pipelines/data_engineering/queries/create_combined_diagnoses_sql.py
@@ -1,7 +1,7 @@
 def combined_diagnoses_query():
  return f'''
              DROP TABLE IF EXISTS derived.combined_diagnoses;
-             CREAT TABLE derived.combined_diagnoses AS
+             CREATE TABLE derived.combined_diagnoses AS
              SELECT "uid" AS "uid", "Diagnoses.label" AS "Diagnoses", "Admissions__ingested_at" AS "ingested_at"
              FROM (SELECT "derived"."exploded_Diagnoses.label"."uid" AS "uid", "derived"."exploded_Diagnoses.label"."Diagnoses.label" AS "Diagnoses.label", "derived"."exploded_Diagnoses.label"."facility" AS "facility", "Admissions"."ingested_at" AS "Admissions__ingested_at" FROM "derived"."exploded_Diagnoses.label"
              LEFT JOIN "derived"."admissions" "Admissions" ON "derived"."exploded_Diagnoses.label"."uid" = "Admissions"."uid")
