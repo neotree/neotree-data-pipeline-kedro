84a74f1b5c48adef143084dccb40cddb2f6b30b0
mbaradza <mbaradza@gmail.com>
2022-12-13 11:30:37 +0200

    Fix Refferred From

--- src/data_pipeline/pipelines/data_engineering/data_tyding/tidy_admissions_discharges_and_create_mcl_tables.py
+++ src/data_pipeline/pipelines/data_engineering/data_tyding/tidy_admissions_discharges_and_create_mcl_tables.py
@@ -281,6 +281,8 @@ def tidy_tables():
         set_key_to_none(baseline_df,'BWGroup.value') 
         set_key_to_none(baseline_df,'AdmittedFrom.label') 
         set_key_to_none(baseline_df,'AdmittedFrom.value') 
+         set_key_to_none(baseline_df,'ReferredFrom2.label') 
+        set_key_to_none(baseline_df,'ReferredFrom2.value') 
         #Vital Signs Table
         format_date(vit_signs_df,'D1Date.value')
         format_date(vit_signs_df,'TimeTemp1.value')
