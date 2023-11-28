4f4e1f45e20ff4cd03a794cf7a1448bd56701e18
mbaradza <mbaradza1@gmail.com>
2023-11-28 12:13:51 +0200

    Restructure Indexes on New and Old Admissions

--- src/data_pipeline/pipelines/data_engineering/derive_data/create_union_views.py
+++ src/data_pipeline/pipelines/data_engineering/derive_data/create_union_views.py
@@ -308,9 +308,8 @@ def union_views():
             # SAVE OLD NEW ADMISSIONS
             try:
                 if new_smch_admissions is not None and old_smch_admissions  is not None:
-                    new_smch_admissions= new_smch_admissions.reset_index(inplace=True,drop=True)
-                    old_smch_admissions= old_smch_admissions.reset_index(inplace=True,drop=True)
-                    combined_adm_df = pd.concat([new_smch_admissions, old_smch_admissions],axis=0,ignore_index=True).reset_index(inplace=True, drop=True)
+                    old_smch_admissions= old_smch_admissions.reset_index(drop=True)
+                    combined_adm_df = pd.concat([new_smch_admissions, old_smch_admissions],axis=0,ignore_index=True)
                     if not combined_adm_df.empty:   
                         catalog.save('create_derived_old_new_admissions_view',combined_adm_df)  
             except Exception as e:
@@ -320,9 +319,8 @@ def union_views():
             # SAVE OLD NEW DISCHARGES
             try:
                 if new_smch_discharges  is not None and old_smch_discharges  is not None:
-                    new_smch_discharges= new_smch_discharges.reset_index(inplace=True, drop=True)
-                    old_smch_discharges = old_smch_discharges.reset_index(inplace=True, drop=True) 
-                    combined_dis_df = pd.concat([new_smch_discharges, old_smch_discharges],axis=0,ignore_index=True).reset_index(inplace=True, drop=True)
+                    old_smch_discharges = old_smch_discharges.reset_index(drop=True) 
+                    combined_dis_df = pd.concat([new_smch_discharges, old_smch_discharges],axis=0,ignore_index=True)
                     if not combined_dis_df.empty:   
                         catalog.save('create_derived_old_new_discharges_view',combined_dis_df)  
             except Exception as e:
@@ -335,7 +333,6 @@ def union_views():
                 if new_smch_matched_data  is not None and old_matched_smch_data  is not None:
                     #Correct UID column to suit the lower case uid in new_smch_matched_data
                     if 'UID' in old_matched_smch_data.columns:
-                        old_matched_smch_data=old_matched_smch_data.reset_index(drop=True)
                         new_smch_matched_data = new_smch_matched_data.reset_index(drop=True)
                         old_matched_smch_data = old_matched_smch_data.rename(columns = {'UID': 'uid'})  
                     combined_matched_df = pd.concat([new_smch_matched_data, old_matched_smch_data],axis=0,ignore_index=True)
