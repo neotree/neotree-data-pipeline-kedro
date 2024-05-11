ee634ac6e84cf9dfb54edae11d6a8d000fc16393
mbaradza <mbaradza1@gmail.com>
2024-05-11 23:09:59 +0200

    Fix erronous values

--- src/data_pipeline/pipelines/data_engineering/utils/data_label_fix_new.py
+++ src/data_pipeline/pipelines/data_engineering/utils/data_label_fix_new.py
@@ -1,15 +1,18 @@
 
 from  conf.common.scripts import get_script
+import logging
 
 def fix_data_label(key,value,script):
-    
-    script = get_script(script)
-    if(script is not None):
-        item = script.get(key)
-        
-        if item:
-            if value in item["values"]:
-                value_index = item["values"].index(value)
-                label = item["labels"][value_index]
-                return label
-    return None
+    try:
+        script_json = get_script(script)
+        if(script_json is not None):
+            item = script_json.get(key)
+            
+            if item:
+                if value in item["values"]:
+                    value_index = item["values"].index(value)
+                    label = item["labels"][value_index]
+                    return label
+        return None
+    except Exception as ex:
+        logging.error("**********"+str(key)+"-----"+str(value)+"+++++"+str(script))
