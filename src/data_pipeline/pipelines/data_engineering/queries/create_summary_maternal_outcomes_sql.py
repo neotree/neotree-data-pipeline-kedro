6816dad5aa799b5a1afd6be7584d171e243df3b8
mbaradza <mbaradza1@gmail.com>
2024-05-12 15:01:00 +0200

    Fix Pattern Match Error For Numbers

--- src/data_pipeline/pipelines/data_engineering/queries/create_summary_maternal_outcomes_sql.py
+++ src/data_pipeline/pipelines/data_engineering/queries/create_summary_maternal_outcomes_sql.py
@@ -3,8 +3,7 @@ from conf.base.catalog import params
 def summary_maternal_outcomes_query():
     #Defaulting to Malawi Case 
     gestation_case = f''' CASE
-        WHEN derived.maternal_outcomes."Gestation.value" IS NULL 
-        OR derived.maternal_outcomes."Gestation.value" !~ '^[0-9]*\.?[0-9]+$' THEN 'Unkown'
+        WHEN derived.maternal_outcomes."Gestation.value" IS NULL THEN 'Unkown'
         WHEN derived.maternal_outcomes."Gestation.value" < 28 THEN '<28wks'
         WHEN derived.maternal_outcomes."Gestation.value" >= 28 AND derived.maternal_outcomes."Gestation.value" < 32 THEN '28-32wks'
         WHEN derived.maternal_outcomes."Gestation.value" >= 32 AND derived.maternal_outcomes."Gestation.value" < 34 THEN '34-34wks'
@@ -14,8 +13,7 @@ def summary_maternal_outcomes_query():
         END AS "GestationGroup" '''
     if('country' in params and str(params['country']).lower()) =='zimbabwe':
         gestation_case= f''' CASE
-        WHEN derived.maternal_outcomes."Gestation.value" IS NULL 
-        OR derived.maternal_outcomes."Gestation.value" !~ '^[0-9]*\.?[0-9]+$' THEN 'Unkown'
+        WHEN derived.maternal_outcomes."Gestation.value" IS NULL THEN 'Unkown'
         WHEN derived.maternal_outcomes."Gestation.value" < 28 THEN '<28 weeks'
         WHEN derived.maternal_outcomes."Gestation.value" >= 28 AND derived.maternal_outcomes."Gestation.value" < 32 THEN '28-31 weeks'
         WHEN derived.maternal_outcomes."Gestation.value" >= 32 AND derived.maternal_outcomes."Gestation.value" < 34 THEN '32-33 weeks'
@@ -59,8 +57,7 @@ def summary_maternal_outcomes_query():
         case when derived.maternal_outcomes."DateAdmission.value" IS NOT NULL THEN 1 End AS "BirthCount",
         {gestation_case},
         CASE
-         WHEN derived.maternal_outcomes."BWTDis.value" IS NULL 
-        OR derived.maternal_outcomes."BWTDis.value" !~ '^[0-9]*\.?[0-9]+$' THEN 'Unknown'
+         WHEN derived.maternal_outcomes."BWTDis.value" IS NULL THEN 'Unknown'
         WHEN derived.maternal_outcomes."BWTDis.value" < 1000 THEN '<1000g'
         WHEN derived.maternal_outcomes."BWTDis.value" >= 1000 AND derived.maternal_outcomes."BWTDis.value" < 1500 THEN '1000-1500g'
         WHEN derived.maternal_outcomes."BWTDis.value" >= 1500 AND derived.maternal_outcomes."BWTDis.value" < 2500 THEN '1500-2500g'
@@ -69,8 +66,7 @@ def summary_maternal_outcomes_query():
         WHEN derived.maternal_outcomes."BWTDis.value" >= 4000 THEN '>4000g'
         END AS "BirthWeightGroup",
         CASE
-         WHEN derived.maternal_outcomes."Gestation.value" IS NULL 
-        OR derived.maternal_outcomes."Gestation.value" !~'^[0-9]*\.?[0-9]+$' THEN 7
+         WHEN derived.maternal_outcomes."Gestation.value" IS NULL THEN 7
         WHEN derived.maternal_outcomes."Gestation.value" < 28 THEN 1
         WHEN derived.maternal_outcomes."Gestation.value" >= 28 AND derived.maternal_outcomes."Gestation.value" < 32 THEN 2
         WHEN derived.maternal_outcomes."Gestation.value" >= 32 AND derived.maternal_outcomes."Gestation.value" < 34 THEN 3
@@ -79,8 +75,7 @@ def summary_maternal_outcomes_query():
         WHEN derived.maternal_outcomes."Gestation.value" >= 42 THEN 6
         END AS "GestationGroupSort",
         CASE
-         WHEN derived.maternal_outcomes."BWTDis.value" IS NULL 
-        OR derived.maternal_outcomes."BWTDis.value" !~'^[0-9]*\.?[0-9]+$' THEN 7
+         WHEN derived.maternal_outcomes."BWTDis.value" IS NULL THEN 7
         WHEN derived.maternal_outcomes."BWTDis.value" < 1000 THEN 1
         WHEN derived.maternal_outcomes."BWTDis.value" >= 1000 AND derived.maternal_outcomes."BWTDis.value" < 1500 THEN 2
         WHEN derived.maternal_outcomes."BWTDis.value" >= 1500 AND derived.maternal_outcomes."BWTDis.value" < 2500 THEN 3
