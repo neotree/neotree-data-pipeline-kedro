7a0ec31d2740b6c20cecf12a0ba2eba9362cf4ec
mbaradza <mbaradza1@gmail.com>
2024-07-20 06:32:26 +0200

    Remove None Values

--- src/data_pipeline/pipelines/data_engineering/data_tyding/regenerate_unique_key.py
+++ src/data_pipeline/pipelines/data_engineering/data_tyding/regenerate_unique_key.py
@@ -38,8 +38,6 @@ def regenerate_unique_key():
                 
                 if len(values)>0:
                     query = regenerate_unique_key_query(id,values[0])
-                else:
-                    query = regenerate_unique_key_query(id,None)
                 if index<20 or index>285460:    
                     logging.info("-QUERY ="+query)
                 inject_sql(query,"UNIQUE-KEYS")
