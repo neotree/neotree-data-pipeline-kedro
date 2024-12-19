7f08a20a3c4651021eca4ae6f970bf89f9b4c1eb
mbaradza <mbaradza1@gmail.com>
2024-12-19 18:35:57 +0200

    ....

--- src/data_pipeline/pipelines/data_engineering/data_tyding/tidy_admissions_discharges_and_create_mcl_tables.py
+++ src/data_pipeline/pipelines/data_engineering/data_tyding/tidy_admissions_discharges_and_create_mcl_tables.py
@@ -305,10 +305,12 @@ def tidy_tables():
         
         ##################################DISCHARGES SCRIPT#################################################### 
         dis_df = pd.json_normalize(dis_new_entries)
-        if not dis_df.empty:
-            uid_set = set(adm_df['uid'])
+        if not dis_df.empty and not adm_df.empty:
+            uid_set=[]
+            if 'uid' adm_df and 'uid' in dis_df:
+                uid_set = set(adm_df['uid'])
             #ONLY TAKE DISCHARGES WITH CORRESPONDING ADMISSIONS
-            dis_df = dis_df[dis_df['uid'].isin(uid_set)].drop_duplicates(subset='uid')
+                dis_df = dis_df[dis_df['uid'].isin(uid_set)].drop_duplicates(subset='uid')
             if "started_at" in dis_df and 'completed_at' in dis_df :
                 dis_df = format_date_without_timezone(dis_df,['started_at','completed_at'])
                 dis_df['time_spent'] = (dis_df['completed_at'] -dis_df['started_at']).astype('timedelta64[m]')
