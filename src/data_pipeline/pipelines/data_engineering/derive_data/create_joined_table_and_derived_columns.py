41819599705450723aeb4dda0d14dfd31185b070
mbaradza <mbaradza1@gmail.com>
2025-01-09 10:17:12 +0200

    ..

--- src/data_pipeline/pipelines/data_engineering/derive_data/create_joined_table_and_derived_columns.py
+++ src/data_pipeline/pipelines/data_engineering/derive_data/create_joined_table_and_derived_columns.py
@@ -39,31 +39,31 @@ def join_table():
         adm_df['DEDUPLICATER'] =adm_df['DateTimeAdmission.value'].map(lambda x: str(x)[:10] if len(str(x))>=10 else None) 
         dis_df['DEDUPLICATER'] =dis_df['DateTimeAdmission.value'].map(lambda x: str(x)[:10] if len(str(x))>=10 else None) 
 
-        duplicates_admissions = adm_df[adm_df.duplicated(subset=['uid', 'facility'], keep=False)]
+        # duplicates_admissions = adm_df[adm_df.duplicated(subset=['uid', 'facility'], keep=False)]
 
-        unique_record_admissions = adm_df[~adm_df.index.isin(duplicates_admissions.index)]
+        # unique_record_admissions = adm_df[~adm_df.index.isin(duplicates_admissions.index)]
    
-        duplicates_discharges = dis_df[dis_df[['uid', 'facility']]
-                                .isin(unique_record_admissions[['facility', 'uid']]
-                                .to_dict(orient='list')).all(axis=1)]
+        # duplicates_discharges = dis_df[dis_df[['uid', 'facility']]
+        #                         .isin(unique_record_admissions[['facility', 'uid']]
+        #                         .to_dict(orient='list')).all(axis=1)]
 
-        unique_record_discharges =  dis_df[~dis_df.index.isin(duplicates_discharges.index)]
+        # unique_record_discharges =  dis_df[~dis_df.index.isin(duplicates_discharges.index)]
 
 
-        jn_adm_dis = unique_record_admissions.merge(
-        unique_record_discharges, 
+        jn_adm_dis = adm_df.merge(
+        dis_df, 
         how='inner', 
         on=['uid', 'facility'], 
         suffixes=('', '_discharge')
         )
-        duplicate_adm_dis = duplicates_admissions.merge(
-        duplicates_discharges, 
-        how='inner', 
-        on=['uid', 'facility','DEDUPLICATER'], 
-        suffixes=('', '_discharge')
-        )
+        # duplicate_adm_dis = duplicates_admissions.merge(
+        # duplicates_discharges, 
+        # how='inner', 
+        # on=['uid', 'facility','DEDUPLICATER'], 
+        # suffixes=('', '_discharge')
+        # )
 
-        jn_adm_dis = pd.concat([jn_adm_dis, duplicate_adm_dis], ignore_index=True)
+        # jn_adm_dis = pd.concat([jn_adm_dis, duplicate_adm_dis], ignore_index=True)
 
         # Drop helper columns if needed
         if table_exists('derived','joined_admissions_discharges'):
