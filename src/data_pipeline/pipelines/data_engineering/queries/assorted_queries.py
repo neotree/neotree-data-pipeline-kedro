6536a53c6225105c1372d6b9e8cdba656bb8c1b9
mbaradza <mbaradza1@gmail.com>
2025-03-18 15:44:47 +0200

    ...

--- src/data_pipeline/pipelines/data_engineering/queries/assorted_queries.py
+++ src/data_pipeline/pipelines/data_engineering/queries/assorted_queries.py
@@ -236,8 +236,8 @@ def read_data_with_no_unique_key():
     return f'''
              SELECT 
              id,
-            "data"->'entries' AS "entries",
-            "data"->'appVersion' AS "appVersion"
+            "data"->>'entries' AS "entries",
+            "data"->>'appVersion' AS "appVersion"
             FROM public.clean_sessions
             WHERE "unique_key" not like '%-%-%';'''
 
