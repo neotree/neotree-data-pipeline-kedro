24dbed0f98beebdb9e4f85de399a54fc94bbb19b
mbaradza <mbaradza1@gmail.com>
2025-03-18 18:45:52 +0200

    ....

--- conf/common/sql_functions.py
+++ conf/common/sql_functions.py
@@ -5,14 +5,11 @@ from sqlalchemy import create_engine,text
 import sys
 from sqlalchemy.types import TEXT
 import pandas as pd
-import psycopg2
+
 
 params = config()
 #Postgres Connection String
-con = 'postgresql+psycopg2://' + \
-params["user"] + ':' + params["password"] + '@' + \
-params["host"] + ':' + '5432' + '/' + params["database"]
-# engine = create_engine(con, executemany_mode='batch')
+con = f'''postgresql://{params["user"]}:{params["password"]}@{params["host"]}:5432/{params["database"]}'''
 
 engine = create_engine(
     con,
@@ -163,13 +160,11 @@ def column_exists(schema, table_name,column_name):
         
 def run_query_and_return_df(query):
     try:
-        con_string = f'''postgresql://{params["user"]}:{params["password"]}@{params["host"]}:5432/{params["database"]}'''
-        conn = psycopg2.connect(con_string)
+        
         logging.info('***CONNECTION ESTABLISHED*******************')
-        df = pd.read_sql_query(query, conn)
+        df = pd.read_sql_query(query, engine)
         logging.info(df.head())
         return df
     except Exception as ex:
         logging.error(formatError(ex))
-    finally:
-        conn.close()
+  
