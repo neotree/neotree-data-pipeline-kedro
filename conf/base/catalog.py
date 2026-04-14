2959fbec589de0b8dc2e80d750bdd4fb49e118eb
KumaloWilson <kumalowilson900@gmail.com>
2026-04-14 16:54:26 +0200

    revert kedro to v 17

--- conf/base/catalog.py
+++ conf/base/catalog.py
@@ -25,13 +25,10 @@ cron_time = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
 start = time.time()
 
 cwd = os.getcwd()
-logs_dir = str(cwd+"/logs")
+logs_dir = os.path.join(cwd, "logs")
+Path(logs_dir).mkdir(parents=True, exist_ok=True)
 log = logging.getLogger('');
-#Prefered Log Var for Ubuntu
-ubuntu_log_dir = "/var/log"
-if Path(ubuntu_log_dir).exists():
-    logs_dir = ubuntu_log_dir
-cron_log_file = Path(logs_dir+'/data_pipeline_cron.log');
+cron_log_file = Path(logs_dir) / "data_pipeline_cron.log"
 
 generic_dedup_queries = []
 
